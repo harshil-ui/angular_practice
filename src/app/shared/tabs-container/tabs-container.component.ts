f7b0147d85a0e1e4826fcb4461d77ecfd7ed06e8
harshil <MY_NAME@example.com>
2023-01-05 08:30:33 +0530

    component design completed

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabsComponent) tabs?: QueryList<TabsComponent>;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
   }
 
+  selectTab(tab: TabsComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false;
+    })
+
+    tab.active = true;
+
+    return false
+  }
 }
